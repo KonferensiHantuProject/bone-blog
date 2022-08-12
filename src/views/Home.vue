d65815df9573ae27793246ab14588766de14cb28
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-08-12 20:09:31 +0700

    Using Refs as Reactive Value

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="home">
     Home
+
+    <p>Nama saya adalah {{ name }} umurku {{ age }}</p>
+    <button @click="handleClick">Klik</button>
+    <button @click="age++">Tambah 1 Umur</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 
 export default {
   name: 'Home',
   setup() {
-    
+    console.log('set up')
+
+    // const p = ref(null)
+
+    const name = ref('Mario')
+    const age = ref(45)
+
+    const handleClick = () => {
+      name.value = 'Luigi'
+      age.value = 35
+    }
+
+    return {
+      name,
+      age,
+      handleClick,
+    }
   }
 }
 </script>
