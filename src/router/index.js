74f39a1f421198f4e515484daebaef757135e9da
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-08-14 10:28:47 +0700

    Adding Posts and Props

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SecondHome from '../views/SecondHome.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/second',
+    name: 'Second Home',
+    component: SecondHome
   }
 ]
 
---
+++ src/views/SecondHome.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+        <h1>Home</h1>
+        <PostList :posts="posts" />
+    </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import PostList from '../components/PostList.vue'
+
+export default {
+    name: 'Home Second',
+    components: {
+        PostList
+    },
+    setup() {
+        const posts = ref([
+            { title: 'Selamat Datang', body: 'lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor lorep ipsum dolor', id: 1},
+            { title: 'Selamat Datang Lagi', body: 'lorep ipsum dolor number two', id: 2},
+        ])
+
+        return {
+            posts
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
